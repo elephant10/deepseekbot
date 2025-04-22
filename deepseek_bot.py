4e59e813a72b6c11d13686c86412c2e059bf28c4
elephant10 <bsevavv@gmail.com>
2025-04-22 23:49:25 +0300

    trylly asynch

--- deepseek_bot.py
+++ deepseek_bot.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from openai import AsyncOpenAI 
 from dotenv import load_dotenv
 load_dotenv()
 import os
@@ -10,20 +11,20 @@ class Deepseek :
         api_key = api_key or os.environ.get("deepseek_key")
         if api_key is None:
             raise ValueError("API key is required")
-        self.client = OpenAI(api_key=api_key, base_url=Deepseek.base_url)
+        self.client = AsyncOpenAI(api_key=api_key, base_url=Deepseek.base_url)
 
-    def chat(self, message: list, model:str="deepseek-chat"):
+    async def chat(self, message: list, model:str="deepseek-chat"):
         system_prompt = ""
         with open(r"system_prompt.txt", "r") as f:
                     system_prompt = f.read()
         
-        response = self.client.chat.completions.create(
+        response = await self.client.chat.completions.create(
             model=model,
             messages=[
                 {"role": "system", "content": system_prompt}] +
                     message,
             stream=False,
-            temperature=2,
+            temperature=0.9,
             max_tokens=8192
 
         )
